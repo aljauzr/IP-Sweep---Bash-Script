4d3648f5a70b34698f90b6de02cc99ae76095f95
Al Jauzi Abdurrohman <72987214+aljauzr@users.noreply.github.com>
2023-10-02 00:22:19 +0700

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 if [ "$1" == " " ]
 then
-echo "You forgot an IP address!"
-echo "Syntax: ./ipsweep.sh 192.168.1"
+  echo "You forgot an IP address!"
+  echo "Syntax: ./ipsweep.sh 192.168.1"
 
 else
-for range in `seq 1 254`; do
-ping $1.$range -c 1 | grep "64 bytes" | cut -f 4 -d " " | tr -d ":" &
+  for range in `seq 1 254`; do
+  ping $1.$range -c 1 | grep "64 bytes" | cut -f 4 -d " " | tr -d ":" &
 done
 fi
