0083dfb8e8ed6a532b3b3107ab806966ea55ec95
Al Jauzi Abdurrohman <72987214+aljauzr@users.noreply.github.com>
2023-10-03 06:43:39 +0700

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+
 if [ "$1" == " " ]
 then
   echo "You forgot an IP address!"
@@ -8,4 +9,5 @@ else
   for range in `seq 1 254`; do
   ping $1.$range -c 1 | grep "64 bytes" | cut -f 4 -d " " | tr -d ":" &
 done
+
 fi
