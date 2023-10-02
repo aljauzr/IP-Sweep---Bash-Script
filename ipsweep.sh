8643cfff7b84f2001b412b999ccd48092f503df8
Al Jauzi Abdurrohman <72987214+aljauzr@users.noreply.github.com>
2023-10-03 06:45:37 +0700

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -7,7 +7,7 @@ then
 
 else
   for range in `seq 1 254`; do
-  ping $1.$range -c 1 | grep "64 bytes" | cut -f 4 -d " " | tr -d ":" &
-done
+    ping $1.$range -c 1 | grep "64 bytes" | cut -f 4 -d " " | tr -d ":" &
+  done
 
 fi
